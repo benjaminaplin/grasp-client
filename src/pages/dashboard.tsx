ada39c8508a81d92220ac6e06f6bb258e8d4165d
benjaminaplin <benjaminaplin@gmail.com>
2024-05-07 18:18:32 -0400

    change userId

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,95 +1,111 @@
-import { DashboardCard } from "../components/dashboard-card/DashboardCard"
-import Layout from "../components/layout/Layout"
-import Typography from "@mui/material/Typography"
+import { DashboardCard } from '../components/dashboard-card/DashboardCard'
+import Layout from '../components/layout/Layout'
+import Typography from '@mui/material/Typography'
 import '../styles/dashboard.css'
-import { Contact } from "../types/contact"
-import { Application } from "../types/application"
-import { useQueryWrapper } from "../context/WrapUseQuery"
-import { Company } from "../types/company"
-import { NextStep } from "../types/next-step"
-import { Interview } from "../types/interview"
+import { Contact } from '../types/contact'
+import { Application } from '../types/application'
+import { useQueryWrapper } from '../context/WrapUseQuery'
+import { Company } from '../types/company'
+import { NextStep } from '../types/next-step'
+import { Interview } from '../types/interview'
 
 export const Dashboard = () => {
-  const { data: companies } = useQueryWrapper<Company[]>(`companies`)
-  const { data: applications  } = useQueryWrapper<Application[]>(`job-applications`)
-  const { data: nextSteps  } = useQueryWrapper<NextStep[]>(`next-steps`)
-  const { data: contacts  } = useQueryWrapper<Contact[]>(`contacts`)
-  const { data: interviews  } = useQueryWrapper<Interview[]>(`interviews`)
+  const { data: companies } = useQueryWrapper<Company[]>(`users/3/companies`)
+  const { data: applications } =
+    useQueryWrapper<Application[]>(`job-applications`)
+  const { data: nextSteps } = useQueryWrapper<NextStep[]>(`next-steps`)
+  const { data: contacts } = useQueryWrapper<Contact[]>(`contacts`)
+  const { data: interviews } = useQueryWrapper<Interview[]>(`interviews`)
   const { data: touches } = useQueryWrapper<Contact[]>(`touches`)
 
-  const recruiters = contacts?.filter((contact: Contact) => contact.type === 'Recruiter')?.length || 0
-  const recruiterApplications = applications?.filter((application: Application) => application.type?.includes('recruiter'))?.length || 0
-  const coldApplications = applications?.filter((application: Application) => application.type?.includes('cold'))?.length || 0
-  const connectionApplications = applications?.filter((application: Application) => application.type?.includes('connection'))?.length || 0
+  const recruiters =
+    contacts?.filter((contact: Contact) => contact.type === 'Recruiter')
+      ?.length || 0
+  const recruiterApplications =
+    applications?.filter((application: Application) =>
+      application.type?.includes('recruiter'),
+    )?.length || 0
+  const coldApplications =
+    applications?.filter((application: Application) =>
+      application.type?.includes('cold'),
+    )?.length || 0
+  const connectionApplications =
+    applications?.filter((application: Application) =>
+      application.type?.includes('connection'),
+    )?.length || 0
 
   return (
-    <Layout title="Dashboard" >
+    <Layout title='Dashboard'>
       <div className='stat-cards-container'>
         <DashboardCard
           title='Applications'
           color='coral'
           destinationOnClick='/job-applications'
           content={<StatContainer>{applications?.length || 0}</StatContainer>}
-          />
+        />
         <DashboardCard
           title='Recruiter Applications'
           color='green-blue-light'
           destinationOnClick='/job-applications'
           content={<StatContainer>{recruiterApplications}</StatContainer>}
-          />
+        />
         <DashboardCard
           title='Cold Applications'
           color='green-blue-light'
           destinationOnClick='/job-applications'
           content={<StatContainer>{coldApplications}</StatContainer>}
-          />
+        />
         <DashboardCard
           title='Connection Applications'
           color='green-blue-light'
           destinationOnClick='/job-applications'
           content={<StatContainer>{connectionApplications}</StatContainer>}
-          />
+        />
         <DashboardCard
-          title='Contacts' 
+          title='Contacts'
           color='verdigris'
           destinationOnClick='/contacts'
-          content={<StatContainer>{contacts?.length  || 0}</StatContainer>} 
-          />
+          content={<StatContainer>{contacts?.length || 0}</StatContainer>}
+        />
         <DashboardCard
-          title='Recruiters' 
+          title='Recruiters'
           color='verdigris'
           destinationOnClick='/contacts'
-          content={<StatContainer>{recruiters}</StatContainer>} 
-          />
+          content={<StatContainer>{recruiters}</StatContainer>}
+        />
         <DashboardCard
           title='Companies'
-          color='verdigris' 
+          color='verdigris'
           destinationOnClick='/companies'
           content={<StatContainer>{companies?.length || 0}</StatContainer>}
-          />
+        />
         <DashboardCard
-          title='Next Steps' 
+          title='Next Steps'
           color='verdigris'
           destinationOnClick='/next-steps'
-          content={<StatContainer>{nextSteps?.length || 0}</StatContainer>} 
-          />
+          content={<StatContainer>{nextSteps?.length || 0}</StatContainer>}
+        />
         <DashboardCard
-          title='Interviews' 
+          title='Interviews'
           color='picton-blue'
           destinationOnClick='/interviews'
-          content={<StatContainer>{interviews?.length || 0}</StatContainer>} 
-          />
+          content={<StatContainer>{interviews?.length || 0}</StatContainer>}
+        />
         <DashboardCard
-          title='Touches' 
+          title='Touches'
           color='picton-blue'
           destinationOnClick='/touches'
-          content={<StatContainer>{touches?.length || 0}</StatContainer>} 
-          />
+          content={<StatContainer>{touches?.length || 0}</StatContainer>}
+        />
       </div>
     </Layout>
   )
 }
 
 const StatContainer = ({ children }: { children: number }) => {
-  return <Typography align='center' variant='h4'>{children}</Typography>
+  return (
+    <Typography align='center' variant='h4'>
+      {children}
+    </Typography>
+  )
 }
