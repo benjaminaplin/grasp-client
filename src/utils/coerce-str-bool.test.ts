b3f91f56903cece0388167e38fa95778ea457afa
benjaminaplin <benjaminaplin@gmail.com>
2024-05-08 12:28:54 -0400

    add tests

--- src/utils/coerce-str-bool.test.ts
+++ src/utils/coerce-str-bool.test.ts
@@ -1,5 +1,12 @@
-describe('test', () => {
-  it('tests', () => {
-    expect(true).toBe(true)
+import { describe, it } from 'vitest'
+import { coerceStringToBool } from './coerce-str-bool'
+
+describe('coerceStringToBool', () => {
+  it('coerces a stringified boolean to a boolean', () => {
+    expect(coerceStringToBool('true')).toBe(true)
+    expect(coerceStringToBool('false')).toBe(false)
+  })
+  it('returns the value as is if string is not stringified boolean', () => {
+    expect(coerceStringToBool('not a bool')).toBe('not a bool')
   })
 })
